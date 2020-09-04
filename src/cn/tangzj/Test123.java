2184b34ce87c32b0d2cfec293530663c1ab753f6
tang735162941 <735162941@qq.com>
2020-09-04 18:38:54 +0800

    GitHub1

---
+++ src/cn/tangzj/Test123.java
@@ -0,0 +1,13 @@
+package cn.tangzj;
+
+public class Test123 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("987654");
+	}
+
+}
